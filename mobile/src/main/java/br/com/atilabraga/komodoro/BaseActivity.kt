4a0ada6c921a1a778f0027fd47aa376dc10338ec
atilabraga <braga.atila@gmail.com>
2016-08-26 00:40:12 -0300

    Making BaseActivity abstract

--- mobile/src/main/java/br/com/atilabraga/komodoro/BaseActivity.kt
+++ mobile/src/main/java/br/com/atilabraga/komodoro/BaseActivity.kt
@@ -3,15 +3,13 @@ package br.com.atilabraga.komodoro
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-open class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(getContentView())
     }
 
-    open fun getContentView() : Int {
-        return 0
-    }
+    abstract fun getContentView() : Int
 
 }
